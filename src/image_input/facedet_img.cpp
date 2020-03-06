c156bc1a11a43853d14aff5de956c962bbe0f4a6
kurshakuz <abilkasov@gmail.com>
2020-03-06 12:46:23 +0600

    implement isolate function

--- src/image_input/facedet_img.cpp
+++ src/image_input/facedet_img.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 using namespace cv::face;
 
 void detectFaceEyesAndDisplay( Mat frame );
-// void detectAAMAndDisplay( Mat img );
+void isolate( Mat frame, vector<Point2f> landmarks);
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 
@@ -46,6 +46,59 @@ int main( int argc, const char** argv )
     return 0;
 }
 
+void isolate( Mat frame, vector<Point2f> landmarks) 
+{
+    Point region[1][20];
+
+    int points[6] = {36, 37, 38, 39, 40, 41}; // left eye
+    for (int i = 0; i < 6; i++) {
+        // cout << landmarks[points[i]].x << std::endl ;
+        region[0][i] = Point(landmarks[points[i]].x, landmarks[points[i]].y);
+    }
+
+    Size size = frame.size();
+    int height = size.height;
+    int width = size.width;
+
+    cv::Mat black_frame = cv::Mat(height, width, CV_8UC1, Scalar::all(0));
+    cv::Mat mask = cv::Mat(height, width, CV_8UC1, Scalar::all(255));
+
+    int npt[] = { 6 };
+    const Point* ppt[1] = { region[0] };
+    cv::fillPoly(mask, ppt, npt, 1, cv::Scalar(0, 0, 0), 8);
+
+    // cv::Mat eye = frame.clone();
+    // cv::bitwise_not(black_frame, eye, mask = mask);
+
+
+    cv::Mat eye;
+    cv::bitwise_not(black_frame, eye, mask = mask);
+
+    int margin = 5;
+    int x_vals[] = {region[0][0].x, region[0][1].x, region[0][2].x, region[0][3].x, region[0][4].x, region[0][5].x};
+    int y_vals[] = {region[0][0].y, region[0][1].y, region[0][2].y, region[0][3].y, region[0][4].y, region[0][5].y};
+    int min_x = *std::min_element(x_vals, x_vals+6) - margin;
+    int max_x = *std::max_element(x_vals, x_vals+6) + margin;
+    int min_y = *std::min_element(y_vals, y_vals+6) - margin;
+    int max_y = *std::max_element(y_vals, y_vals+6) + margin;
+
+    cout << min_y << std::endl;
+    cout << max_y << std::endl;
+
+    frame = eye(Range(min_y, max_y), Range(min_x, max_x));
+    Point origin = Point(min_x, min_y);
+
+    cout << frame.size() << std::endl;
+
+    Size new_size = frame.size();
+    int new_height = new_size.height;
+    int new_width = new_size.width;
+    int center[] = {new_width / 2, new_height / 2};
+
+    // cout << "frame = " << endl << " "  << frame << endl << endl;
+
+    imshow("Capture - Face detection", frame);
+}
 
 void detectFaceEyesAndDisplay( Mat frame )
 {
@@ -86,16 +139,18 @@ void detectFaceEyesAndDisplay( Mat frame )
     cout << "Loaded facemark LBF model" << endl;
 
     cv::rectangle(frame, faces[0], Scalar(255, 0, 0), 2);
-    vector<vector<Point2f> > shapes;
+    vector<vector<Point2f>> shapes;
     
     if (facemark -> fit(frame, faces, shapes)) {
         // Draw the detected landmarks
         drawFacemarks(frame, shapes[0], cv::Scalar(0, 0, 255));
     }    
 
-    cout <<  shapes[0] << std::endl ;
+    cout <<  shapes[0].size() << std::endl ;
 
-    //-- Show what you got
-    imshow( "Capture - Face detection", frame );
-    // imshow( "Capture - Face detection", faceROI );
+    // cout <<  shapes[0][0].x << std::endl ;
+
+    isolate(frame, shapes[0]);
+
+    // imshow( "Capture - Face detection", frame );
 }
